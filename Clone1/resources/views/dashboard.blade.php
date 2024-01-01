1a600294e2fac0626e493df8b1b5fdafa5c100ac
Landrian61 <luswataandrew190@gmail.com>
2024-01-01 20:57:43 +0300

    Layout Adjustments

--- Clone1/resources/views/dashboard.blade.php
+++ Clone1/resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 <div class ="container">
     <div class="row p-5">
         <div class="col-3 p-5">
-         <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D" class="rounded-circle" alt="">
+         <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D" style=" height:500px;" class="rounded-circle" alt="">
         </div>
         <div class="col-9">
 
