653246c1c4808066a4b56cf451b636c3eb8b2399
Landrian61 <luswataandrew190@gmail.com>
2024-01-03 23:58:29 +0300

    Refactor dashboard.blade.php layout

--- Clone1/resources/views/dashboard.blade.php
+++ Clone1/resources/views/dashboard.blade.php
@@ -2,25 +2,57 @@
     <div class="container">
         <div class="row p-5">
             <div class="col-3 p-5 flex justify-center items-center">
-                <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D" style="height: 500px;" class="rounded-full" alt="">
-            </div>
-            <div class="col-9 pt-5">
+                <img src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D" style="height: 400px; width: 400px; border-radius: 50%;" alt="" class="img-fluid">
+            
+            <div class="col-9 p-5">
                 <div>
-                    <h1>Crunched</h1>
+                    <h1  class="pl-2" style="font-size: 30px; font-weight: bold;">Crunched</h1>
                 </div>
-                <div class="d-flex">
-                    <div class="pr-5"><strong>153</strong> posts</div>
-                    <div class="pr-5"><strong>23k</strong> followers</div>
-                    <div class="pr-5"><strong>212</strong> following</div>
+                <div class="flex flex-row justify-content-between">
+                    <div class="pl-5"><strong>153</strong> posts</div>
+                    <div class="pl-5"><strong>23k</strong> followers</div>
+                    <div class="pl-5"><strong>212</strong> following</div>
                 </div>
-                <div class="pt-4 font-weight-bold">Crunched.org</div>
-                <div>We are a global team of millions of people learning to code together!</div>
-                <div><a href="#">www.crunched.org</a></div>
+                <div class="p-2 font-weight-bold" style="color: blue; font-size: 20px;">Crunched.org</div>
+                <div class="pl-2 font-weight-bold">We are a global team of millions of people learning to code together!</div>
+                <div class="p-2 font-weight-bold"><a href="#" class="hover-blue">www.crunched.org</a></div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-4">
-                <!-- Add your posts section here -->
+            </div>
+            <div class="col-4 text-center">
+                <h2 class="text-bold text-dark pl-40" style="font-size: 24px;">POSTS</h2>
+            </div>
+        <div class="flex flex-row justify-content-center align-items-center pl-40">
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1089938556/photo/developing-programmer-team-development-website-design-and-coding-technologies-working-in.jpg?s=612x612&w=0&k=20&c=xi_SQa-7onZjcvPW3eNiSOsiVl-HvkBiqmrHpAtcQis=" alt="Image 1" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1347879996/photo/shot-of-a-young-woman-using-a-laptop-while-working-in-a-server-room.jpg?s=612x612&w=0&k=20&c=xe2m3QQoTVoPllIrEjq3fNYfa43reyO0A6my6N8EHbE=" alt="Image 2" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1291751221/photo/the-sales-department-scores-again.jpg?s=612x612&w=0&k=20&c=m5TBkQ4fpqEjX372FqL6pQ3I4oGLFdYQVtK-kgZc_Lw=" alt="Image 3" class="img-fluid">
+            </div>
+        </div>
+        <div class="flex flex-row justify-content-center align-items-center pl-40">
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1089938556/photo/developing-programmer-team-development-website-design-and-coding-technologies-working-in.jpg?s=612x612&w=0&k=20&c=xi_SQa-7onZjcvPW3eNiSOsiVl-HvkBiqmrHpAtcQis=" alt="Image 1" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1347879996/photo/shot-of-a-young-woman-using-a-laptop-while-working-in-a-server-room.jpg?s=612x612&w=0&k=20&c=xe2m3QQoTVoPllIrEjq3fNYfa43reyO0A6my6N8EHbE=" alt="Image 2" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1291751221/photo/the-sales-department-scores-again.jpg?s=612x612&w=0&k=20&c=m5TBkQ4fpqEjX372FqL6pQ3I4oGLFdYQVtK-kgZc_Lw=" alt="Image 3" class="img-fluid">
+            </div>
+        </div>
+        <div class="flex flex-row justify-content-center align-items-center pl-40">
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1089938556/photo/developing-programmer-team-development-website-design-and-coding-technologies-working-in.jpg?s=612x612&w=0&k=20&c=xi_SQa-7onZjcvPW3eNiSOsiVl-HvkBiqmrHpAtcQis=" alt="Image 1" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1347879996/photo/shot-of-a-young-woman-using-a-laptop-while-working-in-a-server-room.jpg?s=612x612&w=0&k=20&c=xe2m3QQoTVoPllIrEjq3fNYfa43reyO0A6my6N8EHbE=" alt="Image 2" class="img-fluid">
+            </div>
+            <div class="col-4 image-container hover:scale-105 transition duration-300 ease-in-out" style="padding: 10px;">
+                <img src="https://media.istockphoto.com/id/1291751221/photo/the-sales-department-scores-again.jpg?s=612x612&w=0&k=20&c=m5TBkQ4fpqEjX372FqL6pQ3I4oGLFdYQVtK-kgZc_Lw=" alt="Image 3" class="img-fluid">
             </div>
         </div>
     </div>
